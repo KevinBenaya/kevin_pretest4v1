7a81aa4022b485a1ed138393a566383c6a608ac6
KevinBenaya <darkfaust063@gmail.com>
2022-03-13 12:06:33 +0700

    main.dart(kevin_pretest4v1)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,38 +8,75 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFFFFCE00),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(4),
+              ),
+            ),
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
+          ),
+        ),
+        unselectedWidgetColor: const Color(0x50333333),
+        shadowColor: const Color(0xFFe6e6e6).withOpacity(0.5),
+        backgroundColor: Colors.white,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: 'Poppins',
+        textTheme: TextTheme(
+          headline1: GoogleFonts.poppins(
+            color: const Color(0xFF111111),
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+          headline2: GoogleFonts.poppins(
+            color: const Color(0xFF111111),
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+          headline3: GoogleFonts.poppins(
+            color: const Color(0xFF111111),
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+          headline4: GoogleFonts.poppins(
+            color: const Color(0xFF111111),
+            fontSize: 12,
+            fontWeight: FontWeight.bold,
+          ),
+          headline5: GoogleFonts.poppins(
+            color: const Color(0xFF111111),
+            fontSize: 12,
+            fontWeight: FontWeight.w500,
+          ),
+          headline6: GoogleFonts.poppins(
+            color: Theme.of(context).unselectedWidgetColor,
+            fontSize: 10,
+            fontWeight: FontWeight.w500,
+          ),
+          bodyText1: GoogleFonts.poppins(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+          bodyText2: GoogleFonts.poppins(
+            color: const Color(0xFF111111),
+            fontSize: 9,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(),
-      ),
+      home: const Scaffold(),
     );
   }
 }
